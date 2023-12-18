439059a94731b258d0384571ab39ad09f664a589
paunov0000 <legendarygriefer96@gmail.com>
2023-12-18 12:53:23 +0200

    added some css, now fetches and looks quite good

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,33 +1,46 @@
-// const MAX_POKEMON = 151;
-// const pokemonWrapperDiv = document.querySelector(".pokemon-wrapper");
-// const url = `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}&offset=0`;
-// fetch(url)
-//   .then((res) => res.json())
-//   .then((data) => {
-//     // debugger;
-//     for (const currPokemon of data.results) {
-//       const cardPokemon = document.createElement("div");
-//       const h3PokemonName = document.createElement("h3");
-//       const h5PokemonId = document.createElement("h5");
-//       const imgPokemonContainer = document.createElement("div");
-//       const imgPokemon = document.createElement("img");
+const MAX_POKEMON = 151;
+const pokemonWrapperDiv = document.querySelector(".pokemon-wrapper");
+const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}&offset=0`;
+const imgUrl = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/`;
+fetch(baseUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    // debugger;
+    for (const currPokemon of data.results) {
+      const cardPokemon = document.createElement("div");
+      cardPokemon.classList.add("pokemon-card");
 
-//       h3PokemonName.textContent = currPokemon.name;
-//       pokemonIdExtractor(currPokemon).then(
-//         (pokemonId) => (h5PokemonId.textContent = pokemonId)
-//       );
+      const pokemonNameContainer = document.createElement("div");
+      pokemonNameContainer.classList.add("pokemon-name-container");
 
-//       cardPokemon.appendChild(h3PokemonName);
-//       cardPokemon.appendChild(h5PokemonId);
-//       pokemonWrapperDiv.appendChild(cardPokemon);
-//     }
-//   });
+      const pPokemonName = document.createElement("p");
+      pPokemonName.classList.add("pokemon-name");
 
-// function pokemonIdExtractor(pokemon) {
-//   const url = `https://pokeapi.co/api/v2/pokemon/${pokemon.name}`;
-//   const pokemonId = fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => data.id);
+      const pPokemonId = document.createElement("p");
+      pPokemonId.classList.add("pokemon-id");
 
-//   return pokemonId;
-// }
+      const imgPokemonContainer = document.createElement("div");
+      imgPokemonContainer.classList.add("pokemon-img");
+
+      const imgPokemon = document.createElement("img");
+
+      pPokemonName.textContent = currPokemon.name;
+      const pokemonId = pokemonIdExtractor(currPokemon.url);
+      pPokemonId.textContent = pokemonId;
+
+      imgPokemon.src = `${imgUrl}${pokemonId}.svg`;
+      imgPokemonContainer.appendChild(imgPokemon);
+      pokemonNameContainer.appendChild(pPokemonName);
+
+      cardPokemon.appendChild(pPokemonId);
+      cardPokemon.appendChild(imgPokemonContainer);
+      cardPokemon.appendChild(pokemonNameContainer);
+
+      pokemonWrapperDiv.appendChild(cardPokemon);
+    }
+  });
+
+function pokemonIdExtractor(pokemonUrl) {
+  const id = pokemonUrl.split(`/`)[6];
+  return id;
+}
