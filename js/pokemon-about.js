0c87a57ae2fe96f74ee5f6b19f4ba311e3d90161
paunov0000 <legendarygriefer96@gmail.com>
2023-12-27 11:24:47 +0200

    in the process of making it generate its' elements based on fetch info rather than replace html

--- js/pokemon-about.js
+++ js/pokemon-about.js
@@ -1,4 +1,6 @@
 const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+const body = document.querySelector("body");
+body.innerHTML = ``;
 
 // Function to fetch data based on the provided id
 function fetchData(id) {
@@ -8,9 +10,178 @@ function fetchData(id) {
 
 // Function to render the fetched data
 function renderData(data) {
+  //create the elements
+  body.innerHTML = `<div class="card">
+  <header>
+    <div class="arrow-back">
+      <img src="./assetsv2/arrowBack.svg" alt="back-arrow" />
+    </div>
+    <div id="pokemon-name">
+    </div>
+    <div id="pokemon-id">
+    </div>
+  </header>
+
+  <main>
+    <div id="pokemon-display">
+      <div id="previous-pokemon">
+        <img src="./assetsv2/arowPrevious.svg" alt="" />
+      </div>
+      <div id="pokemon-image">
+        <img
+          src="https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+          alt=""
+        />
+      </div>
+      <div id="next-pokemon">
+        <img src="./assetsv2/arrowNext.svg" alt="" />
+      </div>
+    </div>
+    <div id="pokemon-type">
+    </div>
+    <div id="pokemon-about">
+      <p>About</p>
+    </div>
+    <div id="pokemon-info">
+      <div id="pokemon-dimensions">
+        <div class="pokemon-stat">
+          <div id="weight" class="img-stat">
+            <div class="img-container">
+              <img src="./assetsv2/pokemonWeight.svg" alt="" />
+            </div>
+            <p>9.9kg</p>
+          </div>
+          <p class="measurement">Weight</p>
+        </div>
+        <div class="pokemon-stat">
+          <div id="height" class="img-stat">
+            <div id="ruler" class="img-container">
+              <img src="./assetsv2/pokemonHeight.svg" alt="" />
+            </div>
+            <p>9.9m</p>
+          </div>
+          <p class="measurement">Height</p>
+        </div>
+        <div id="pokemon-abilties">
+          <div class="ability">
+            <p>Ability 1</p>
+            <p>Ability 2</p>
+          </div>
+          <p class="measurement">Moves</p>
+        </div>
+      </div>
+    </div>
+    <div id="pokemon-description">
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed
+        nostrum minima in quis quisquam incidunt labore enim ut laboriosam
+        tenetur ipsa explicabo, excepturi cum esse qui totam minus nam
+        laudantium.
+      </p>
+    </div>
+    <div id="pokemon-stats">
+      <div id="stats-title">
+        <p>Base Stats</p>
+      </div>
+      <div id="hp-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>HP</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="atk-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>ATK</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="def-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>DEF</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="satk-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>SATK</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="sdef-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>SDEF</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="spd-stat" class="stat-cell">
+        <div class="stat-name">
+          <p>SPD</p>
+        </div>
+        <div class="stat-bar">
+          <div class="stat-value">
+            <p>999</p>
+          </div>
+          <div class="container">
+            <div class="progress2 progress-moved">
+              <div class="progress-bar2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</div>`;
+
   //render the data into the details.html page
-  const pokemonName = document.querySelector("#pokemon-name p");
-  const pokemonId = document.querySelector("#pokemon-id p");
+  const pokemonName = document.querySelector("#pokemon-name");
+  const pokemonId = document.querySelector("#pokemon-id");
+  const pokemonPrevious = document.querySelector("#previous-pokemon");
+  const pokemonNext = document.querySelector("#next-pokemon");
   const pokemonImg = document.querySelector("#pokemon-image img");
   const pokemonTypeContainer = document.querySelector("#pokemon-type");
   const pokemonWeight = document.querySelector("#weight p");
@@ -25,11 +196,31 @@ function renderData(data) {
   const pokemonSpd = document.querySelector("#spd-stat p:nth-child(2)");
 
   //set the values of the elements
-  pokemonName.textContent = capitalizeFirstLetter(data.name);
-  pokemonId.textContent = `#${padWithLeadingZeros(data.id, 3)}`;
+  const pPokemonName = document.createElement("p");
+  pPokemonName.textContent = capitalizeFirstLetter(data.name);
+  pokemonName.appendChild(pPokemonName);
+
+  const pPokemonId = document.createElement("p");
+  pPokemonId.textContent = `#${padWithLeadingZeros(data.id, 3)}`;
+  pokemonId.appendChild(pPokemonId);
+  pokemonPrevious.addEventListener("click", () => {
+    window.location.href = `details.html?id=${data.id - 1}`;
+  });
+  pokemonNext.addEventListener("click", () => {
+    window.location.href = `details.html?id=${data.id + 1}`;
+  });
   pokemonImg.src = data.sprites.other.dream_world.front_default;
-  pokemonWeight.textContent = `Weight: ${data.weight}`;
-  pokemonHeight.textContent = `Height: ${data.height}`;
+
+  for (let index = 0; index < data.types.length; index++) {
+    const currTypeName = data.types[index].type.name;
+
+    const currTypeElement = document.createElement("p");
+    currTypeElement.textContent = capitalizeFirstLetter(currTypeName);
+    pokemonTypeContainer.appendChild(currTypeElement);
+  }
+
+  pokemonWeight.textContent = `Weight: ${data.weight} kg`;
+  pokemonHeight.textContent = `Height: ${data.height} m`;
 
   //loop through the types array and add the types to the pokemonTypes element
   for (const currType of data.types) {
