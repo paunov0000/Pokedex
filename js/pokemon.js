6a1555a1892840bc7ff7f08421e38bd49006f3ce
paunov0000 <legendarygriefer96@gmail.com>
2024-01-05 17:19:12 +0200

    made it work w/o making a request for the filtering ;p

--- js/pokemon.js
+++ js/pokemon.js
@@ -3,26 +3,28 @@ const pokemonWrapperDiv = document.querySelector(".pokemon-wrapper");
 const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}&offset=0`;
 const imgUrl = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/`;
 const searchBar = document.querySelector("#searchBar");
+let pokemon = [];
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
-  // const filteredPokemon = pokemon.filter((pokemon) => {
-  //   return pokemon.name.toLowerCase().includes(searchString);
-  // });
-  // displayPokemon(filteredPokemon);
-  fetch(baseUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      const filteredPokemon = data.results.filter((pokemon) => {
-        return pokemon.name.toLowerCase().startsWith(searchString);
-      });
+  const filteredPokemon = pokemon.filter((pokemon) => {
+    return pokemon.name.toLowerCase().includes(searchString);
+  });
+  displayPokemon(filteredPokemon);
+  // fetch(baseUrl)
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     const filteredPokemon = data.results.filter((pokemon) => {
+  //       return pokemon.name.toLowerCase().startsWith(searchString);
+  //     });
 
-      displayPokemon(filteredPokemon);
-    });
+  //     displayPokemon(filteredPokemon);
+  //   });
 });
 fetch(baseUrl)
   .then((res) => res.json())
   .then((data) => {
     // debugger;
+    pokemon = data.results;
     displayPokemon(data.results);
     // for (const currPokemon of data.results) {
     //   const cardPokemon = document.createElement("div");
