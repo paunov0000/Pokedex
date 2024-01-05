65fc749a8553250649becf4f8076297507fca002
paunov0000 <legendarygriefer96@gmail.com>
2024-01-05 17:19:52 +0200

    removed unnecessary comments

--- js/pokemon.js
+++ js/pokemon.js
@@ -4,65 +4,20 @@ const baseUrl = `https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}&offset=0
 const imgUrl = `https://raw.githubusercontent.com/pokeapi/sprites/master/sprites/pokemon/other/dream-world/`;
 const searchBar = document.querySelector("#searchBar");
 let pokemon = [];
+
 searchBar.addEventListener("keyup", (e) => {
   const searchString = e.target.value.toLowerCase();
   const filteredPokemon = pokemon.filter((pokemon) => {
     return pokemon.name.toLowerCase().includes(searchString);
   });
   displayPokemon(filteredPokemon);
-  // fetch(baseUrl)
-  //   .then((res) => res.json())
-  //   .then((data) => {
-  //     const filteredPokemon = data.results.filter((pokemon) => {
-  //       return pokemon.name.toLowerCase().startsWith(searchString);
-  //     });
-
-  //     displayPokemon(filteredPokemon);
-  //   });
 });
+
 fetch(baseUrl)
   .then((res) => res.json())
   .then((data) => {
-    // debugger;
     pokemon = data.results;
     displayPokemon(data.results);
-    // for (const currPokemon of data.results) {
-    //   const cardPokemon = document.createElement("div");
-    //   cardPokemon.classList.add("pokemon-card");
-
-    //   const pokemonNameContainer = document.createElement("div");
-    //   pokemonNameContainer.classList.add("pokemon-name-container");
-
-    //   const pPokemonName = document.createElement("p");
-    //   pPokemonName.classList.add("pokemon-name");
-
-    //   const pPokemonId = document.createElement("p");
-    //   pPokemonId.classList.add("pokemon-id");
-
-    //   const imgPokemonContainer = document.createElement("div");
-    //   imgPokemonContainer.classList.add("pokemon-img");
-
-    //   const imgPokemon = document.createElement("img");
-
-    //   pPokemonName.textContent = capitalizeFirstLetter(currPokemon.name);
-    //   const pokemonId = pokemonIdExtractor(currPokemon.url);
-    //   pPokemonId.textContent = `#${padWithLeadingZeros(pokemonId, 3)}`; //! Add leading 0s
-
-    //   imgPokemon.src = `${imgUrl}${pokemonId}.svg`;
-    //   imgPokemonContainer.appendChild(imgPokemon);
-    //   pokemonNameContainer.appendChild(pPokemonName);
-
-    //   cardPokemon.id = pokemonId;
-
-    //   cardPokemon.appendChild(pPokemonId);
-    //   cardPokemon.appendChild(imgPokemonContainer);
-    //   cardPokemon.appendChild(pokemonNameContainer);
-    //   cardPokemon.addEventListener("click", () => {
-    //     window.location.href = `./details.html?id=${pokemonId}`;
-    //   });
-
-    //   pokemonWrapperDiv.appendChild(cardPokemon);
-    // }
   });
 
 function pokemonIdExtractor(pokemonUrl) {
